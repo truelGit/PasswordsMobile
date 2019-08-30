4cf2b29f47e3029ea75c9e1abb209ad2ed81bae2
Alexander Ivichev <ivichev@gmail.com>
2019-08-30 19:40:15 +0300

    Add project files.

---
+++ Countr.iOS/Views/CountersView.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Countr.Core.ViewModels;
+using MvvmCross.Binding.BindingContext;
+using MvvmCross.iOS.Views;
+using UIKit;
+
+namespace Countr.iOS.Views
+{
+    [MvxFromStoryboard]
+    public partial class CountersView : MvxTableViewController
+    {
+        public CountersView(IntPtr handle) : base(handle)
+        {
+        }
+
+        public override void ViewDidLoad()
+        {
+            base.ViewDidLoad();
+
+            var source = new CountersTableViewSource(TableView);
+            TableView.Source = source;
+
+            var button = new UIBarButtonItem(UIBarButtonSystemItem.Add);
+            NavigationItem.SetRightBarButtonItem(button, false);
+
+            var set = this.CreateBindingSet<CountersView, CountersViewModel>();
+            set.Bind(source).To(vm => vm.Counters);
+            set.Bind(button).To(vm => vm.ShowAddNewCounterCommand);
+            set.Apply();
+        }
+    }
+}
+
